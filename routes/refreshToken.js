4aa46c17abfcbc636945197ba35b8314f9a130cc
Mayank <mayankarora0877@gmail.com>
2023-11-22 17:34:00 +0530

    final

--- routes/refreshToken.js
+++ routes/refreshToken.js
@@ -25,6 +25,28 @@ router.post('/generate', async(req, res)=>{
 
 })
 
+// logout
+router.delete('/', async(req, res)=> {
+    try {
+        const { error } = refreshTokenValidation(req.body);
+        if (error)
+            return res
+                .status(400)
+                .json({ error: true, message: error.details[0].message });
+
+        const userToken = await UserToken.findOne({ token: req.body.refreshToken });
+        if (!userToken)
+            return res
+                .status(200)
+                .json({ error: false, message: "Logged Out Sucessfully" });
+
+        await userToken.remove();
+        res.status(200).json({ error: false, message: "Logged Out Sucessfully" });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: true, message: "Internal Server Error" });
+    }
+});
 
 
 module.exports = router;
